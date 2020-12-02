4f5269f5ae768860a802dbcf4b48b4c2b5d2af0d
fadooda <fadyibrahim37@gmail.com>
2020-12-02 15:50:04 -0500

    removed trim since there won't be any user input of space

--- users.js
+++ users.js
@@ -2,8 +2,6 @@
 const users = [];
 
 const addUser = ({ id, name, room }) => {
-  name = name.trim().toLowerCase();
-  room = room.trim().toLowerCase();
 
   const existingUser = users.find((user) => user.room === room && user.name === name);
 
